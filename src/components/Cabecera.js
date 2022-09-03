5e12e16c3a08c5e986cf454698ed8f7e50650620
juayala96 <juayala96@gmail.com>
2022-09-03 00:20:32 -0300

    cambios finales

--- src/components/Cabecera.js
+++ src/components/Cabecera.js
@@ -4,7 +4,7 @@ export default function Cabecera({cantidad}) {
   return (
     <header>
         <h1>Carrito de compras</h1>
-        <p>Cantidad de productos: <span>{cantidad}</span></p>
+        <p>Cantidad de productos <span>{cantidad}</span></p>
     </header>
   )
 }
